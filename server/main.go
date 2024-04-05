5efdf201a56a28ed20149b44995ef287fdff2b6c
Zane Kissel <zekissel@gmail.com>
2024-04-05 02:16:52 -0400

    optimize redirect server

--- server/main.go
+++ server/main.go
@@ -3,16 +3,12 @@ import "net/http"
 import "log"
 
 
-func handleRedirect(w http.ResponseWriter, r *http.Request) {
-	state := r.URL.Query().Get("oauth_state_id")
-	uri := "https://localhost:1420/callback?oauth_state_id=" + state
-	
-	http.Redirect(w, r, uri, http.StatusSeeOther)
-}
-
 func main () {
 
-	http.HandleFunc("/callback", handleRedirect)
+	http.HandleFunc("/callback", func (w http.ResponseWriter, r *http.Request) {
+		uri := "http://localhost:1421/callback?" + r.URL.RawQuery
+		http.Redirect(w, r, uri, http.StatusSeeOther)
+	})
 	
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil { log.Fatal(err) }
